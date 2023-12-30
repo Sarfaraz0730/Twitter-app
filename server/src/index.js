1445acb5fe24fb7582741572f1b70335772a9112
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-12-30 20:56:34 +0530

    work in progess

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,18 @@
-console.log("Main index")
+const express = require('express');
+require('dotenv').config();
+const userRoutes = require('./routes/User-routes/signin.js');
+const home = require("./routes/Home-route/home.js")
+
+var cors = require('cors')
+const app = express();
+const PORT =process.env.PORT;
+
+
+app.use(cors())
+app.use(userRoutes)
+app.use(home)
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is Listening on PORT ${PORT}`)
+})
