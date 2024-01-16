b7a32a6c59d6d99cb6b8c88c562afeaf17d0ddad
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2024-01-16 18:55:07 +0530

    fixed delete post logic

--- server/src/routes/Tweets/deleteTweet.js
+++ server/src/routes/Tweets/deleteTweet.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const PostTweetModel = require('../../model/TweetModel/PostTweetModel');
+const verify = require('../../helper/verify');
 const router = express.Router()
 
-router.delete("/:id",async(req,res,next)=>{
+router.delete("/:id",verify,async(req,res,next)=>{
+    const userId = req.decoded._id
     console.log("params", req.params)
     const postId = req.params.id
     if(!postId){
@@ -13,8 +15,17 @@ router.delete("/:id",async(req,res,next)=>{
     if(!postDetails){
         return res.status(404).send("postDetails not found by id,may be post already deleted")
     }
+    const tweeterId = postDetails.userId;
+    var isAuthUser = (tweeterId == userId)
+    console.log("check",isAuthUser )
+  if(isAuthUser){
     await PostTweetModel.findByIdAndDelete({_id:postId})
-     return res.status(200).send("Post deleted Successfully")
+    return res.status(200).send("Post deleted Successfully")
+  }
+  return res.status(404).json({message:"The user who has posted the tweet and the user who is trying to delete the post is not same, due to that you cannot delete the post"})
+ 
+
+
     }catch(err){
      return res.status(500).send("internal server error")
     }
