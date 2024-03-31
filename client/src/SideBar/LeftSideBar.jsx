a6d9e058bd5de47099062c9ad728bc871c1bd0bf
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2024-03-31 12:43:36 +0530

    improving ui

--- client/src/SideBar/LeftSideBar.jsx
+++ client/src/SideBar/LeftSideBar.jsx
@@ -1,31 +1,40 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faSearch, faBell, faEnvelope ,faUser} from '@fortawesome/free-solid-svg-icons';
-import "./Style.SideBar.css"
+import { faHome, faSearch, faBell, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
+import xlog from "../images/x-logo.jpg";
+import "./Style.SideBar.css";
+
 const LeftSideBar = () => {
   return (
     <div className="left-sidebar">
-      {/* Your sidebar content goes here */}
-      <div className="sidebar-item">
-        <FontAwesomeIcon icon={faHome} /> Home
+      <div className='top-hamburger'>
+        <img className="logo" src={xlog} alt="Logo" />
       </div>
-      <div className="sidebar-item">
-        <FontAwesomeIcon icon={faSearch} /> Explore
+      <div className="sidebar-items">
+        <div className="sidebar-item active">
+          <FontAwesomeIcon icon={faHome} /> <span>Home</span>
+        </div>
+        <div className="sidebar-item">
+          <FontAwesomeIcon icon={faSearch} /> <span>Explore</span>
+        </div>
+        <div className="sidebar-item">
+          <FontAwesomeIcon icon={faBell} /> <span>Notifications</span>
+        </div>
+        <div className="sidebar-item">
+          <FontAwesomeIcon icon={faEnvelope} /> <span>Messages</span>
+        </div>
+        <div className="sidebar-item">
+          <FontAwesomeIcon icon={faUser} /> <span>Profile</span>
+        </div>
       </div>
-      <div className="sidebar-item">
-        <FontAwesomeIcon icon={faBell} /> Notifications
+      <div className='bottom-div'>
+        <div className='user-profile-pic'></div>
+        <div className='user-details'>
+          <div className='user-name'>Sarfaraz</div>
+          <div className='username'>username</div>
+        </div>
       </div>
-      <div className="sidebar-item">
-        <FontAwesomeIcon icon={faEnvelope} /> Messages
-      </div>
-   
-      <div className="sidebar-item">
-        <FontAwesomeIcon icon={faUser} /> Profile
-   
 
-      </div>
-      {/* Add more sidebar items as needed */}
-      
     </div>
   );
 }
